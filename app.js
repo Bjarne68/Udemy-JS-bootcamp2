82ace495ced34ef75f78202b033e746e58c0fab9
Bjarne68 <bjan2100@student.miun.se>
2022-12-01 16:28:54 -0300

    Lektion 24

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-let snowMan = {
-  isSnowman: true,
-  color: "white",
-  madeOfSnowballs: 3,
-  "first name": "Frosty",
+let home = {
+  rooms: 4,
+  apartment: false,
+  color: "brown",
+  swimmingPool: true,
+  state: "WA",
+  City: "Seattle",
+};
+function Home(rooms, apartment, color, swimmingPool, state, City) {
+  this.rooms = rooms;
+  this.apartment = apartment;
+  this.color = color;
+  this.swimmingPool = swimmingPool;
+  this.state = state;
+  this.City = City;
+}
+
+let home1 = new Home(3, false, "blue", true, "CA", "Atlanta");
+
+console.log(home1);
+let home2 = new Home(4, true, "white", false, "no", "Uppsala");
+let home3 = new Home(5, false, "blue", true, "no", "Stockholm");
+let home4 = new Home(2, true, "white", false, "MG", "Formiga");
+console.log(home2, home3, home4);
+
+let book = {
+  författare: "Pelle Svensson",
+  titel: "Sagan om Ringen",
+  utgivningsår: 2021,
 };
 
-console.log(snowMan.isSnowman);
-console.log(snowMan.color);
-console.log(snowMan.madeOfSnowballs);
-console.log(snowMan["first name"]);
+function Library(författare, titel, utgivningsår) {
+  (this.författare = författare),
+    (this.titel = titel),
+    (this.utgivningsår = utgivningsår);
+}
 
-let house = {};
-house.doors = 2;
-house.name = "big";
-house.nice = true;
-house.color = "red";
+let book1 = new Library("August Stringberg", "dkffjfjkgf", 1930);
+let book2 = new Library("Bo Bosson", "ddkfjffjgfkgf", 2011);
+let book3 = new Library("Erik Eriksson", "sklfkfjf kkfjfjfi", 2010);
+let book4 = new Library("Sven Svensson", "ddmkffnfkjnf", 2011);
+let book5 = new Library("Lars Larsson", "slddfjdfdfj", 2005);
 
-console.log(house);
-house["swimming pool"] = false;
-house["first owner"] = 'Bjarne Andersson';
-house["typeof"] = 'apartment';
-
-console.log(house);
+console.log(book1, book2, book3, book4, book5);
